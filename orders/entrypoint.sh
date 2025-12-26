8743ec38673ca9ad770e50c9e98b6e7fbca51892
nemoymoy <nemoymoy@gmail.com>
2025-12-27 00:12:32 +0400

    again adding entrypoint.sh

--- orders/entrypoint.sh
+++ orders/entrypoint.sh
@@ -2,16 +2,18 @@
 
 if [ "$DATABASE" = "postgres" ]
 then
+    # Если база еще не запущена
     echo "Waiting for postgres..."
-
+    # Проверяем доступность хоста и порта
     while ! nc -z $DJANGO_POSTGRES_HOST $DJANGO_POSTGRES_PORT; do
       sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
-
+# Удаляем все старые данные
 python manage.py flush --no-input
+# Выполняем миграции
 python manage.py migrate
 
 exec "$@"
