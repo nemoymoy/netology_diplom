6a3febeccb2fd08816463d742fe8b44e8fd7e922
nemoymoy <nemoymoy@gmail.com>
2025-12-27 15:13:32 +0400

    fix static and media root

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # То как статический файл будет отображаться в url
 # По какому пути можно будет найти static файлы
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Аналогично с media файлами
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
