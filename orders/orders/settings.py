f508869c016262711ab8f2298398ac5262dbf8b1
nemoymoy <nemoymoy@gmail.com>
2025-12-27 13:54:16 +0400

    adding folders for static and media

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -126,4 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
+# То как статический файл будет отображаться в url
+# По какому пути можно будет найти static файлы
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+# Аналогично с media файлами
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
