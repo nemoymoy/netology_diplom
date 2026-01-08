4a95e979a25cc636300a2db724c667207c79dc97
nemoymoy <nemoymoy@gmail.com>
2026-01-09 00:06:32 +0400

    testing templates html 10

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -163,8 +163,8 @@ STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
